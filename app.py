216480d3d51539cd6fa624dca80bf56ae23f3628
Lázaro Ismael Hardy Llins <lazaro.llins@dynamox.local@NOTE-0460.dynamox.local>
2024-08-02 17:00:02 -0300

    feat/Second commit

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
 from http import HTTPStatus
-from schemas import UserSchemas, UserPublicSchemas, UserDBSchemas, MessageSchemas
+
+from fastapi import FastAPI
+
+from schemas import MessageSchemas, UserDBSchemas, UserSchemas, UserPublicSchemas
 
 app = FastAPI()
 
@@ -15,6 +17,6 @@ def index():
 
 @app.post("/users/", status_code=HTTPStatus.CREATED, response_model=UserPublicSchemas)
 def create_user(user: UserSchemas):
-    user_with_id = UserDBSchemas(id=len(database) + 1, **user.mmodel_dump())
+    user_with_id = UserDBSchemas(id=len(database) + 1, **user.model_dump())
     database.append(user_with_id)
-    return database
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,24 @@ def test_root_deve_retornar_ok():
 
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {"message": "Hello World"}
+
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        "/users/",
+        json={
+            "username": "testusername",
+            "password": "testpassword",
+            "email": "test@testemail",
+        },
+    )
+    # Validate
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        "username": "testusername",
+        "password": "testpassword",
+        "email": "test@testemail",
+        "id": 1,
+    }
